3f8eda41c7e153b200557a81ae697dc367aac0a4
Atkn0 <52462284+Atkn0@users.noreply.github.com>
2024-02-11 00:23:00 +0300

    Update apiService.kt

--- app/src/main/java/com/example/storyapp_kotlin/di/retrofit/apiService.kt
+++ app/src/main/java/com/example/storyapp_kotlin/di/retrofit/apiService.kt
@@ -8,7 +8,7 @@ import retrofit2.http.POST
 interface apiService {
     @Headers(
         "content-type: application/json",
-        "X-RapidAPI-Key: 691c89bcfcmsh17efb1ba3e83c89p1c2684jsn75e7d103a1d1",
+        "X-RapidAPI-Key: YOUR_API_KEY",
         "X-RapidAPI-Host: open-ai21.p.rapidapi.com"
     )
     @POST("texttoimage2")
